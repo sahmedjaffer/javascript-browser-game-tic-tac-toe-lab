ab27b8d528a6dcf273038af3019fee330441f2cf
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-23 18:32:02 +0300

    try

--- js/app.js
+++ js/app.js
@@ -1,27 +1,62 @@
 /*-------------------------------- Constants --------------------------------*/
-let squareEls , messageEl;
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board,turn,winner,tie;
+let board = [],turn='X',winner='',tie=false;
 
 /*------------------------ Cached Element References ------------------------*/
-const sqr1 = document.querySelectorAll('#0');
-const sqr2 = document.querySelectorAll('#1');
-const sqr3 = document.querySelectorAll('#2');
-const sqr4 = document.querySelectorAll('#3');
-const sqr5 = document.querySelectorAll('#4');
-const sqr6 = document.querySelectorAll('#5');
-const sqr7 = document.querySelectorAll('#6');
-const sqr8 = document.querySelectorAll('#7');
-const sqr9 = document.querySelectorAll('#8');
-
+const sqr1 = document.querySelector("#square0");
+const sqr2 = document.querySelector("#square1");
+const sqr3 = document.querySelector("#square2");
+const sqr4 = document.querySelector("#square3");
+const sqr5 = document.querySelector("#square4");
+const sqr6 = document.querySelector("#square5");
+const sqr7 = document.querySelector("#square6");
+const sqr8 = document.querySelector("#square7");
+const sqr9 = document.querySelector("#square8");
+const messageEl=document.querySelector("#message");
+const squareEls=document.querySelectorAll(".sqr");
+messageEl.textContent=`X turn`
+playerTurn(turn);
 /*-------------------------------- Functions --------------------------------*/
+function playerTurn(){
+    squareEls.forEach(sqrs => {
 
+        sqrs.addEventListener('click', (event) => {
+            sqrs.textContent=turn
 
+           // if (sqrs.textContent === '' || sqrs.textContent===null){
+                if (turn === 'X'){
+                    turn = 'O';
+                }else {
+                    turn='X';
+                }
+                console.log(turn);
+                sqrs.textContent=turn
+                messageEl.textContent=`${turn} turn`
 
+         //   }
+
+        })
+    })
+}
 /*----------------------------- Event Listeners -----------------------------*/
+
+
+
+// squareEls.addEventListener('click', (event) => {
+  
+//     // shortcut if statement
+//     (sqr1.textContent === '' || sqr1.textContent === null) && (sqr1.textContent=turn) ;
+    
+
+//     if (sqr1.textContent === '' && sqr2.textContent === '' && sqr3.textContent === ''){
+
+//     }
+//     });
+
 
 
 
