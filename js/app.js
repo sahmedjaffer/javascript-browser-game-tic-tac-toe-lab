a2c3ef7426de99b6c91a34aae989b41798bcfbe3
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-23 14:41:22 +0300

    starting the lab

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*-------------------------------- Constants --------------------------------*/
+let squareEls , messageEl;
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let board,turn,winner,tie;
+
+/*------------------------ Cached Element References ------------------------*/
+const sqr1 = document.querySelectorAll('#0');
+const sqr2 = document.querySelectorAll('#1');
+const sqr3 = document.querySelectorAll('#2');
+const sqr4 = document.querySelectorAll('#3');
+const sqr5 = document.querySelectorAll('#4');
+const sqr6 = document.querySelectorAll('#5');
+const sqr7 = document.querySelectorAll('#6');
+const sqr8 = document.querySelectorAll('#7');
+const sqr9 = document.querySelectorAll('#8');
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
