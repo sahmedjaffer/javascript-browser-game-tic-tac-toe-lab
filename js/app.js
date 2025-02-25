26cc697b67bc30a1a4f6e41054c10eb5136d13fd
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-25 16:16:42 +0300

    update code

--- js/app.js
+++ js/app.js
@@ -7,21 +7,23 @@
 let board = [],turn='O',winner='',tie=false;
 
 /*------------------------ Cached Element References ------------------------*/
-const sqr1 = document.querySelector("#square0");
-const sqr2 = document.querySelector("#square1");
-const sqr3 = document.querySelector("#square2");
-const sqr4 = document.querySelector("#square3");
-const sqr5 = document.querySelector("#square4");
-const sqr6 = document.querySelector("#square5");
-const sqr7 = document.querySelector("#square6");
-const sqr8 = document.querySelector("#square7");
-const sqr9 = document.querySelector("#square8");
+const sqr1 = document.getElementById("#0");
+const sqr2 = document.getElementById("#1");
+const sqr3 = document.getElementById("#2");
+const sqr4 = document.getElementById("#3");
+const sqr5 = document.getElementById("#4");
+const sqr6 = document.getElementById("#5");
+const sqr7 = document.getElementById("#6");
+const sqr8 = document.getElementById("#7");
+const sqr9 = document.getElementById("#8");
 const messageEl=document.querySelector("#message");
 const squareEls=document.querySelectorAll(".sqr");
+
 playerTurn(turn);
 /*-------------------------------- Functions --------------------------------*/
 function playerTurn(){
-    messageEl.textContent=`X turn`
+    messageEl.textContent=`X turn`;
+    let clicksTime=0;
     squareEls.forEach(sqrs => {
         sqrs.addEventListener('click', (event) => {
             messageEl.textContent=`${turn} turn`
@@ -33,20 +35,45 @@ function playerTurn(){
                     console.log(`u chose me ${turn}`);
                 }   
                 sqrs.textContent=turn;
-                board.push(turn);
+                let clickedSqr =sqrs.id;
+                
+                    // console.log(clicksTime=1+parseInt(sqrs.id));
+                    // document.getElementById(`#${sqrs.id}`).textContent = turn;
+                    // console.log(document.getElementById(`#${sqrs.id}`).textContent);
+                
+                playedWin(clickedSqr);
+                console.log(clickedSqr);
+                board.push(clickedSqr);
                 console.log(board);
+
+               // console.log(sqrs.textContent.includes[clickedSqr])
+
+    // console.log(sqr2.textContent);
+    // console.log(sqr3.textContent);
+    // console.log(sqr4.textContent);
+    // console.log(sqr5.textContent);
+    // console.log(sqr6.textContent);
+    // console.log(sqr7.textContent);
+    // console.log(sqr8.textContent);
+    // console.log(sqr9.textContent);
+
                 playedWin();
         })
-        
+  
     })
-   
+
+}
+
+function sqrClickedNum(){
+
+
 }
 
 function playedWin(){
-if (sqr1.textContent === 'X' && sqr2.textContent === sqr1.textContent && sqr3.textContent === sqr1.textContent){
-    winner=true;
-    console.log('I am wining')
-}
+// if (sqr1.innerHTML.innerText === 'X' && sqr2.innerText === sqr1.innerText && sqr3.innerText === sqr1.innerText){
+//     winner=true;
+//     console.log('I am wining')
+// }
     // console.log(sqr1.textContent);
     // console.log(sqr2.textContent);
     // console.log(sqr3.textContent);
